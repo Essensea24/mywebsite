f6fa0975c99f9e42df01ef00da3910a1c5617d95
Essensea24 <sophie.chor24@gmail.com>
2015-08-05 13:36:45 -0700

    removed mailer from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        # UserMailer.welcome(@user).deliver_now
+       
         format.html { redirect_to root_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
